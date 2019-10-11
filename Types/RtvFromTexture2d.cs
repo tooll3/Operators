04462f3263d3b8a56bff4a8140d3d3e6cc735885
cynic <daniel@szymanski.cc>
2019-10-11 15:57:47 +0200

    Add operator that creates render target view for a 2d texture

---
+++ Types/RtvFromTexture2d.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using SharpDX.Direct3D11;
+using T3.Core;
+using T3.Core.Logging;
+using T3.Core.Operator;
+
+namespace T3.Operators.Types
+{
+    public class RtvFromTexture2d : Instance<RtvFromTexture2d>
+    {
+        [Output(Guid = "515E87C9-4CF8-4948-BA64-F6261F7FE5FC")]
+        public readonly Slot<RenderTargetView> RenderTargetView = new Slot<RenderTargetView>();
+
+        public RtvFromTexture2d()
+        {
+            RenderTargetView.UpdateAction = Update;
+        }
+
+        private void Update(EvaluationContext context)
+        {
+            if (!Texture.DirtyFlag.IsDirty)
+                return; // nothing to do
+
+            var resourceManager = ResourceManager.Instance();
+            Texture2D texture = Texture.GetValue(context);
+            if (texture != null)
+            {
+                if (((int)texture.Description.BindFlags & (int)BindFlags.RenderTarget) > 0)
+                {
+                    RenderTargetView.Value?.Dispose();
+                    RenderTargetView.Value = new RenderTargetView(resourceManager._device, texture); // todo: create via resource manager
+                }
+                else
+                {
+                    Log.Warning("Trying to create an render target view for resource which doesn't have the rtv bind flag set");
+                }
+            }
+        }
+
+        [Input(Guid = "73CF7C5D-CF0C-49DB-91E8-DAFE812E0232")]
+        public readonly InputSlot<Texture2D> Texture = new InputSlot<Texture2D>();
+    }
+}
