ae7ab6bee73f710d3d11f71010e7d1ff5b5c9c94
cynic <daniel@szymanski.cc>
2019-10-11 11:32:10 +0200

    Added inputs to output merger op

--- Types/OutputMergerStage.cs
+++ Types/OutputMergerStage.cs
@@ -4,12 +4,12 @@ using T3.Core.Operator;
 
 namespace T3.Operators.Types
 {
-    public class OutputMerger : Instance<OutputMerger>
+    public class OutputMergerStage : Instance<OutputMergerStage>
     {
         [Output(Guid = "CEE8C3F0-64EA-4E4D-B967-EC7E3688DD03")]
         public readonly Slot<Scene> Output = new Slot<Scene>();
 
-        public OutputMerger()
+        public OutputMergerStage()
         {
             Output.UpdateAction = Update;
             Output.DirtyFlag.Trigger = DirtyFlagTrigger.Always; // always render atm
@@ -44,8 +44,8 @@ namespace T3.Operators.Types
 //            UpdateMultiInput(ConstantBuffers, ref _constantBuffers, context);
 //            UpdateMultiInput(ShaderResources, ref _shaderResourceViews, context);
 //            UpdateMultiInput(SamplerStates, ref _samplerStates, context);
-            
-            
+
+
 //            outputMerger.
 
 
@@ -63,22 +63,29 @@ namespace T3.Operators.Types
 //        private Buffer[] _constantBuffers = new Buffer[0];
 //        private ShaderResourceView[] _shaderResourceViews = new ShaderResourceView[0];
 //        private SamplerState[] _samplerStates = new SamplerState[0];
-        
-//        [Input(Guid = "394D374F-2125-4ECB-8A69-CC7B2C3C6CB7")]
-//        public readonly InputSlot<DepthStencilView> DepthStencilView = new InputSlot<DepthStencilView>();
-//        [Input(Guid = "9C131DA6-AD56-4E15-9730-754096B3B765")]
-//        public readonly MultiInputSlot<RenderTargetView> RenderTargetViews = new MultiInputSlot<RenderTargetView>();
-//        [Input(Guid = "0ED97939-643B-445E-879C-E18C4430AA03")]
-//        public readonly MultiInputSlot<UnorderedAccessView> UnorderedAccessViews = new MultiInputSlot<UnorderedAccessView>();
-//        [Input(Guid = "1D5FAAD5-3BE5-426C-B464-AD490EA3D1AA")]
-//        public readonly InputSlot<DepthStencilState> DepthStencilState = new InputSlot<DepthStencilState>();
-//        [Input(Guid = "6C7907D7-70F7-4DB7-83EA-22EE48610994")]
-//        public readonly InputSlot<int> DepthStencilReference = new InputSlot<int>();
-//        [Input(Guid = "E0BC9CF8-42C8-4632-B958-7A96F6D03BA2")]
-//        public readonly InputSlot<BlendState> BlendState = new InputSlot<BlendState>();
-//        [Input(Guid = "CCEE2EC3-586F-4396-8B20-CC99484E1B64")]
-//        public readonly InputSlot<System.Numerics.Vector4> BlendFactor = new InputSlot<System.Numerics.Vector4>();
-//        [Input(Guid = "03166157-1E18-4513-8AF5-398C6F4FCB1E")]
-//        public readonly InputSlot<int> BlendSampleMask = new InputSlot<int>();
+
+        [Input(Guid = "394D374F-2125-4ECB-8A69-CC7B2C3C6CB7")]
+        public readonly InputSlot<DepthStencilView> DepthStencilView = new InputSlot<DepthStencilView>();
+
+        [Input(Guid = "9C131DA6-AD56-4E15-9730-754096B3B765")]
+        public readonly MultiInputSlot<RenderTargetView> RenderTargetViews = new MultiInputSlot<RenderTargetView>();
+
+        [Input(Guid = "0ED97939-643B-445E-879C-E18C4430AA03")]
+        public readonly MultiInputSlot<UnorderedAccessView> UnorderedAccessViews = new MultiInputSlot<UnorderedAccessView>();
+
+        [Input(Guid = "1D5FAAD5-3BE5-426C-B464-AD490EA3D1AA")]
+        public readonly InputSlot<DepthStencilState> DepthStencilState = new InputSlot<DepthStencilState>();
+
+        [Input(Guid = "6C7907D7-70F7-4DB7-83EA-22EE48610994")]
+        public readonly InputSlot<int> DepthStencilReference = new InputSlot<int>();
+
+        [Input(Guid = "E0BC9CF8-42C8-4632-B958-7A96F6D03BA2")]
+        public readonly InputSlot<BlendState> BlendState = new InputSlot<BlendState>();
+
+        [Input(Guid = "CCEE2EC3-586F-4396-8B20-CC99484E1B64")]
+        public readonly InputSlot<System.Numerics.Vector4> BlendFactor = new InputSlot<System.Numerics.Vector4>();
+
+        [Input(Guid = "03166157-1E18-4513-8AF5-398C6F4FCB1E")]
+        public readonly InputSlot<int> BlendSampleMask = new InputSlot<int>();
     }
 }
