a578ff5d5541547906877ba278a7636055377c7b
cynic <daniel@szymanski.cc>
2019-10-11 15:28:04 +0200

    Added blend state and render target blend desc operators

---
+++ Types/BlendStateOp.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using SharpDX.Direct3D11;
+using T3.Core;
+using T3.Core.Operator;
+
+namespace T3.Operators.Types
+{
+    public class BlendStateOp : Instance<BlendStateOp>
+    {
+        [Output(Guid = "6EF6C44B-EE22-4C64-9910-4F7595C41897")]
+        public readonly Slot<BlendState> BlendState = new Slot<BlendState>();
+
+        public BlendStateOp()
+        {
+            BlendState.UpdateAction = Update;
+        }
+
+        private void Update(EvaluationContext context)
+        {
+            BlendState.Value?.Dispose();
+            var blendDesc = new BlendStateDescription()
+                            {
+                                AlphaToCoverageEnable = AlphaToCoverageEnable.GetValue(context),
+                                IndependentBlendEnable = IndependentBlendEnable.GetValue(context),
+                            };
+
+            if (RenderTargets.DirtyFlag.IsDirty)
+            {
+                _connectedDescriptions = RenderTargets.GetCollectedTypedInputs();
+                RenderTargets.DirtyFlag.Clear();
+            }
+
+            for (int i = 0; i < _connectedDescriptions.Count; i++)
+            {
+                blendDesc.RenderTarget[i] = _connectedDescriptions[i].GetValue(context);
+            }
+            BlendState.Value = new BlendState(ResourceManager.Instance()._device, blendDesc); // todo: put into resource manager
+        }
+
+        private List<Slot<RenderTargetBlendDescription>> _connectedDescriptions;
+
+        [Input(Guid = "63D0E4E8-FA00-4059-A11B-6A31E66757DC")]
+        public readonly MultiInputSlot<RenderTargetBlendDescription> RenderTargets = new MultiInputSlot<RenderTargetBlendDescription>();
+        [Input(Guid = "3CA79807-00C9-471A-AC44-525A05740FED")]
+        public readonly InputSlot<bool> AlphaToCoverageEnable = new InputSlot<bool>();
+        [Input(Guid = "873AD863-DEC6-4B4B-9D81-89D5FA11BEEC")]
+        public readonly InputSlot<bool> IndependentBlendEnable = new InputSlot<bool>();
+    }
+}
