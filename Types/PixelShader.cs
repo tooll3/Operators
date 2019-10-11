0de2dd7e06e2222bd9ee618c15bb25cacc06c93d
cynic <daniel@szymanski.cc>
2019-10-11 12:07:04 +0200

    Added pixel shader and pixel shader stage op

---
+++ Types/PixelShader.cs
@@ -0,0 +1,50 @@
+﻿using T3.Core;
+using T3.Core.Operator;
+
+namespace T3.Operators.Types
+{
+    public class PixelShader : Instance<PixelShader>
+    {
+        [Output(Guid = "9C6E72F8-5CE6-42C3-ABAA-1829D2C066C1")]
+        public readonly Slot<SharpDX.Direct3D11.PixelShader> Shader = new Slot<SharpDX.Direct3D11.PixelShader>();
+
+        private uint _pixelShaderResId;
+
+        public PixelShader()
+        {
+            Shader.UpdateAction = Update;
+        }
+
+        private void Update(EvaluationContext context)
+        {
+            var resourceManager = ResourceManager.Instance();
+
+            if (Source.DirtyFlag.IsDirty || EntryPoint.DirtyFlag.IsDirty || DebugName.DirtyFlag.IsDirty)
+            {
+                string sourcePath = Source.GetValue(context);
+                string entryPoint = EntryPoint.GetValue(context);
+                string debugName = DebugName.GetValue(context);
+                _pixelShaderResId = resourceManager.CreatePixelShaderFromFile(sourcePath, entryPoint, debugName,
+                                                                              () => Shader.DirtyFlag.Invalidate());
+            }
+            else
+            {
+                resourceManager.UpdatePixelShaderFromFile(Source.Value, _pixelShaderResId, ref Shader.Value);
+            }
+
+            if (_pixelShaderResId != ResourceManager.NULL_RESOURCE)
+            {
+                Shader.Value = resourceManager.GetPixelShader(_pixelShaderResId);
+            }
+        }
+
+        [Input(Guid = "24646F06-1509-43CE-94C6-EEB608AD97CD")]
+        public readonly InputSlot<string> Source = new InputSlot<string>();
+
+        [Input(Guid = "501338B3-F432-49A5-BDBD-BCF209671305")]
+        public readonly InputSlot<string> EntryPoint = new InputSlot<string>();
+
+        [Input(Guid = "BE9B3DC1-7122-4B3D-B936-CCCF2581B69E")]
+        public readonly InputSlot<string> DebugName = new InputSlot<string>();
+    }
+}
---
+++ Types/PixelShaderStage.cs
@@ -0,0 +1,85 @@
+﻿using SharpDX.Direct3D11;
+using T3.Core;
+using T3.Core.Operator;
+using Buffer = SharpDX.Direct3D11.Buffer;
+
+namespace T3.Operators.Types
+{
+    public class PixelShaderStage : Instance<PixelShaderStage>
+    {
+        [Output(Guid = "76E7AD5D-A31D-4B1F-9C42-B63C5161117C")]
+        public readonly Slot<Scene> Output = new Slot<Scene>();
+
+        public PixelShaderStage()
+        {
+            Output.UpdateAction = Update;
+            Output.DirtyFlag.Trigger = DirtyFlagTrigger.Always; // always render atm
+        }
+
+//        private void UpdateMultiInput<T>(MultiInputSlot<T> input, ref T[] resources, EvaluationContext context)
+//        {
+//            if (input.DirtyFlag.IsDirty)
+//            {
+//                var connectedConstBuffers = input.GetCollectedTypedInputs();
+//                if (connectedConstBuffers.Count != resources.Length)
+//                {
+//                    resources = new T[connectedConstBuffers.Count];
+//                }
+//
+//                for (int i = 0; i < connectedConstBuffers.Count; i++)
+//                {
+//                    resources[i] = connectedConstBuffers[i].GetValue(context);
+//                }
+//
+//                input.DirtyFlag.Clear();
+//            }
+//        }
+
+        private void Update(EvaluationContext context)
+        {
+            var resourceManager = ResourceManager.Instance();
+            var device = resourceManager._device;
+            var deviceContext = device.ImmediateContext;
+            var psStage = deviceContext.PixelShader;
+
+            var ps = PixelShader.GetValue(context);
+
+//            UpdateMultiInput(ConstantBuffers, ref _constantBuffers, context);
+//            UpdateMultiInput(ShaderResources, ref _shaderResourceViews, context);
+//            UpdateMultiInput(SamplerStates, ref _samplerStates, context);
+//            UpdateMultiInput(Uavs, ref _uavs, context);
+
+            if (ps == null)
+                return;
+
+            psStage.Set(ps);
+            psStage.SetConstantBuffers(0, _constantBuffers.Length, _constantBuffers);
+            psStage.SetShaderResources(0, _shaderResourceViews.Length, _shaderResourceViews);
+            psStage.SetSamplers(0, _samplerStates);
+//            psStage.SetUnorderedAccessViews(0, _uavs);
+
+            // unbind resources
+//            for (int i = 0; i < _uavs.Length; i++)
+//                psStage.SetUnorderedAccessView(i, null);
+            for (int i = 0; i < _samplerStates.Length; i++)
+                psStage.SetSampler(i, null);
+            for (int i = 0; i < _shaderResourceViews.Length; i++)
+                psStage.SetShaderResource(i, null);
+            for (int i = 0; i < _constantBuffers.Length; i++)
+                psStage.SetConstantBuffer(i, null);
+        }
+
+        private Buffer[] _constantBuffers = new Buffer[0];
+        private ShaderResourceView[] _shaderResourceViews = new ShaderResourceView[0];
+        private SamplerState[] _samplerStates = new SamplerState[0];
+
+        [Input(Guid = "1B9BE6EB-96C8-4B1C-B854-99B64EAF5618")]
+        public readonly InputSlot<SharpDX.Direct3D11.PixelShader> PixelShader = new InputSlot<SharpDX.Direct3D11.PixelShader>();
+        [Input(Guid = "BE02A84B-A666-4119-BB6E-FEE1A3DF0981")]
+        public readonly MultiInputSlot<Buffer> ConstantBuffers = new MultiInputSlot<Buffer>();
+        [Input(Guid = "50052906-4691-4A84-A69D-A109044B5300")]
+        public readonly MultiInputSlot<ShaderResourceView> ShaderResources = new MultiInputSlot<ShaderResourceView>();
+        [Input(Guid = "C4E91BC6-1691-4EB4-AED5-DD4CAE528149")]
+        public readonly MultiInputSlot<SamplerState> SamplerStates = new MultiInputSlot<SamplerState>();
+    }
+}
