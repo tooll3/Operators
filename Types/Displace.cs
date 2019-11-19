e872eba2f28ee5184c21b69c975178f11bc0eff0
pixtur <thomas.mann@framefield.com>
2019-11-20 00:16:06 +0100

    Added FloatConstBuffer and stub for Displace shader

---
+++ Types/Displace.cs
@@ -0,0 +1,41 @@
+using System.Numerics;
+using SharpDX.Direct3D11;
+using T3.Core.Operator;
+
+namespace T3.Operators.Types
+{
+    public class Displace : Instance<Displace>
+    {
+        [Output(Guid = "0fd9e62e-e9f8-441d-95e4-84a77e368d84")]
+        public readonly Slot<Texture2D> Output = new Slot<Texture2D>();
+
+        
+        [Input(Guid = "08cc84b0-9ede-49d1-bf3f-52b229b7ec55")]
+        public readonly InputSlot<Texture2D> Image = new InputSlot<Texture2D>();
+
+        [Input(Guid = "A9EF5673-9A48-459D-B08C-B03F3C62CC6F")]
+        public readonly InputSlot<Texture2D> DisplaceMap = new InputSlot<Texture2D>();
+
+        
+        [Input(Guid = "fbf48b39-135b-4e5d-bd90-5671d4070d52")]
+        public readonly InputSlot<float> SampleRadius = new InputSlot<float>();
+
+        [Input(Guid = "3aefdcee-7d34-41eb-b999-9bae7b792941")]
+        public readonly InputSlot<float> DisplaceAmount = new InputSlot<float>();
+
+        [Input(Guid = "ca123b90-e609-4df4-a866-421c272c83fd")]
+        public readonly InputSlot<float> DisplaceOffset = new InputSlot<float>();
+        
+        [Input(Guid = "E13050AB-FB47-4EE1-BDED-76AACEBC203F")]
+        public readonly InputSlot<float> SampleCount = new InputSlot<float>();
+
+        [Input(Guid = "E4DF2330-D25F-4BD8-8BA2-104408316985")]
+        public readonly InputSlot<float> ShiftX = new InputSlot<float>();
+        
+        [Input(Guid = "E859E00F-1AFA-4925-8D7B-DD376F74A546")]
+        public readonly InputSlot<float> ShiftY = new InputSlot<float>();
+
+        [Input(Guid = "EEE699C6-51E9-45E9-A1FB-0A7AEA68130A")]
+        public readonly InputSlot<float> Angle = new InputSlot<float>();
+    }
+}
---
+++ Types/FloatConstBuffer.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using SharpDX;
+using SharpDX.Direct3D11;
+using T3.Core;
+using T3.Core.Operator;
+using Buffer = SharpDX.Direct3D11.Buffer;
+
+namespace T3.Operators.Types
+{
+    public class FloatConstBuffer : Instance<FloatConstBuffer>
+    {
+        [Output(Guid = "f5531ffb-dbde-45d3-af2a-bd90bcbf3710")]
+        public readonly Slot<Buffer> Buffer = new Slot<Buffer>();
+
+        public FloatConstBuffer()
+        {
+            Buffer.UpdateAction = Update;
+            Buffer.DirtyFlag.Trigger = DirtyFlagTrigger.Always;
+        }
+
+        private void Update(EvaluationContext context)
+        {
+            var param = Params.GetCollectedTypedInputs();
+            var array = new float[ Math.Max(16, param.Count)];
+
+            if (array.Length == 0)
+                return;
+            
+            for (int i = 0; i < param.Count; i++)
+            {
+                array[i]= param[i].GetValue(context);
+            }
+            
+            var resourceManager = ResourceManager.Instance();
+            var device = resourceManager._device;
+
+            var size = sizeof(float)*array.Length;
+            using (var data = new DataStream(size, true, true))
+            {
+                data.WriteRange(array);
+                data.Position = 0;
+
+                if (Buffer.Value == null)
+                {
+                    var bufferDesc = new BufferDescription
+                                     {
+                                         Usage = ResourceUsage.Default,
+                                         SizeInBytes = size,
+                                         BindFlags = BindFlags.ConstantBuffer
+                                     };
+                    Buffer.Value = new Buffer(device, data, bufferDesc);
+                }
+                else
+                {
+                    device.ImmediateContext.UpdateSubresource(new DataBox(data.DataPointer, 0, 0), Buffer.Value, 0);
+                }
+            }
+            
+            Buffer.Value.DebugName = nameof(FloatConstBuffer);
+        }
+
+        [Input(Guid = "49556D12-4CD1-4341-B9D8-C356668D296C")]
+        public readonly MultiInputSlot<float> Params = new MultiInputSlot<float>();
+    }
+}
