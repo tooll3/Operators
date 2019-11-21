df8fb8e04942617ec158acb2f566b0cd7b3d327c
cynic <daniel@szymanski.cc>
2019-11-22 00:04:38 +0100

    Added generice particle update op and integrated it

--- Types/CopyStructureCount.cs
+++ Types/CopyStructureCount.cs
@@ -30,11 +30,11 @@ namespace T3.Operators.Types
             deviceContext.CopyStructureCount(targetBuffer, DstAlignedByteOffset.GetValue(context), sourceBufferUav);
         }
         
+        [Input(Guid = "3AC041C8-2C75-425A-9935-ED6DB5DA5CD2")]
+        public readonly InputSlot<UnorderedAccessView> SourceBuffer = new InputSlot<UnorderedAccessView>();
         [Input(Guid = "1386A5E3-75E4-4421-A35B-1D5F79B2CD32")]
         public readonly InputSlot<Buffer> TargetBuffer = new InputSlot<Buffer>();
         [Input(Guid = "17ECE64E-FF76-41F5-BF36-762A5BAAB2AF")]
         public readonly InputSlot<int> DstAlignedByteOffset = new InputSlot<int>();
-        [Input(Guid = "3AC041C8-2C75-425A-9935-ED6DB5DA5CD2")]
-        public readonly InputSlot<UnorderedAccessView> SourceBuffer = new InputSlot<UnorderedAccessView>();
     }
 }
---
+++ Types/ParticleUpdate.cs
@@ -0,0 +1,20 @@
+﻿using SharpDX.Direct3D11;
+using T3.Core;
+using T3.Core.Operator;
+using T3.Core.Operator.Helper;
+
+namespace T3.Operators.Types
+{
+    public class ParticleUpdate : Instance<ParticleUpdate>
+    {
+        [Output(Guid = "A2FC8C88-2DD2-4099-A76E-B252032F41CC")]
+        public readonly Slot<Command> Command = new Slot<Command>();
+
+        [Input(Guid = "164DCB7E-25EC-4CA9-A71B-DFD13EC8DC10")]
+        public readonly InputSlot<string> ShaderFilename = new InputSlot<string>();
+        [Input(Guid = "39261664-C9E9-40CA-85D0-726DC8801BF2")]
+        public readonly InputSlot<ParticleSystem> ParticleSystem = new InputSlot<ParticleSystem>();
+        [Input(Guid = "53A2C70B-2523-4F9F-ABF8-2E5B16DDFE00")]
+        public readonly MultiInputSlot<ShaderResourceView> ShaderResources = new MultiInputSlot<ShaderResourceView>();
+    }
+}
