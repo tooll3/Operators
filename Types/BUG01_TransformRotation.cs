c07ad5e6a2b7e0364f76e86c4d1202a9bd0c97a7
pixtur <thomas.mann@framefield.com>
2019-11-23 01:31:15 +0100

    Added bug01-example-transform-orientation

---
+++ Types/BUG01_TransformRotation.cs
@@ -0,0 +1,14 @@
+using SharpDX.Direct3D11;
+using T3.Core.Operator;
+
+namespace T3.Operators.Types
+{
+    public class BUG01_TransformRotation : Instance<BUG01_TransformRotation>
+    {
+        [Output(Guid = "d5cb94a8-e295-45b8-9e6f-b750ebd863a7")]
+        public readonly Slot<Texture2D> Output = new Slot<Texture2D>();
+
+
+    }
+}
+
