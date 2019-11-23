ab0660b5534b8b07aa10c935cac9270f39555363
pixtur <thomas.mann@framefield.com>
2019-11-23 03:22:32 +0100

    Added experiments with beatPulse editing

---
+++ Types/Div.cs
@@ -0,0 +1,30 @@
+using T3.Core.Operator;
+
+namespace T3.Operators.Types
+{
+    public class Div : Instance<Div>
+    {
+        [Output(Guid = "866642e7-17dd-4375-9d5e-2e3747a554c2")]
+        public readonly Slot<float> Result = new Slot<float>();
+
+        public Div()
+        {
+            Result.UpdateAction = Update;
+        }
+
+        private void Update(EvaluationContext context)
+        {
+            var b = B.GetValue(context);
+            Result.Value = b== 0 
+                               ? 0 
+                               : A.GetValue(context) / b;
+        }
+
+
+        [Input(Guid = "70460191-7573-400f-ba88-11878ecc917c")]
+        public readonly InputSlot<float> A = new InputSlot<float>();
+
+        [Input(Guid = "a79a2f16-7a4e-464d-8af4-3e3029ae853e")]
+        public readonly InputSlot<float> B = new InputSlot<float>();
+    }
+}
