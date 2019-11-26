8817088c5048975c985a9bbb4a64435c8b013f70
pixtur <thomas.mann@framefield.com>
2019-11-27 00:04:44 +0100

    Added crash-example and played around with particle effects

---
+++ Types/Crash14ShaderUpdate.cs
@@ -0,0 +1,10 @@
+using T3.Core.Operator;
+
+namespace T3.Operators.Types
+{
+    public class Crash14ShaderUpdate : Instance<Crash14ShaderUpdate>
+    {
+
+    }
+}
+
--- Types/RenderQuad.cs
+++ Types/RenderQuad.cs
@@ -1,4 +1,4 @@
-using T3.Core;
+using System;
 using T3.Core.Operator;
 
 namespace T3.Operators.Types
@@ -6,11 +6,17 @@ namespace T3.Operators.Types
     public class RenderQuad : Instance<RenderQuad>
     {
         [Output(Guid = "1ff16183-13b9-4c8f-a82a-77e8be0c641b")]
-        public readonly Slot<Command> Output = new Slot<Command>();
+        public readonly Slot<T3.Core.Command> Output = new Slot<T3.Core.Command>();
 
-        [Input(Guid = "018DAB29-DB3B-49EE-872B-9042C4C0BCED")]
+
+        [Input(Guid = "018dab29-db3b-49ee-872b-9042c4c0bced")]
         public readonly InputSlot<System.Numerics.Vector4> Color = new InputSlot<System.Numerics.Vector4>();
 
+        [Input(Guid = "105d474f-a54e-4350-8de6-8bfd4dc0b0dd")]
+        public readonly InputSlot<float> Width = new InputSlot<float>();
+
+        [Input(Guid = "a2d39e5b-38c7-4751-bc29-7389f9e9d8e5")]
+        public readonly InputSlot<float> Height = new InputSlot<float>();
     }
 }
 
