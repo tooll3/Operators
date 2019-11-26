be60aa73a8ce2a7f300ad2322ac0c56babf502a5
pixtur <thomas.mann@framefield.com>
2019-11-26 18:12:29 +0100

    Add color to Quad

--- Types/ImageQuadEmitter.cs
+++ Types/ImageQuadEmitter.cs
@@ -14,7 +14,24 @@ namespace T3.Operators.Types
         public readonly InputSlot<string> ShaderFilename = new InputSlot<string>();
         [Input(Guid = "49a41a2a-39a4-46fd-ab70-5c8442a6fd61")]
         public readonly InputSlot<ParticleSystem> ParticleSystem = new InputSlot<ParticleSystem>();
+
         [Input(Guid = "d133b57d-4e2c-432a-8172-599e7f70e79e")]
         public readonly MultiInputSlot<ShaderResourceView> ShaderResources = new MultiInputSlot<ShaderResourceView>();
+
+        [Input(Guid = "892C9FC8-817D-49D5-97D2-CB290C609AEB")]
+        public readonly InputSlot<float> Size = new InputSlot<float>();
+
+        [Input(Guid = "241961F6-27F2-4D1D-85EE-51A819C3AE21")]
+        public readonly InputSlot<float> Mass = new InputSlot<float>();
+
+        [Input(Guid = "6EF0891C-B6CD-4EEC-A570-11C38E889AD5")]
+        public readonly InputSlot<float> LifeTime = new InputSlot<float>();
+
+        [Input(Guid = "59213885-EBBB-41CF-A6BC-B0A4B657D22B")]
+        public readonly InputSlot<float> EmitPosY = new InputSlot<float>();
+
+        [Input(Guid = "F5FA3F1E-33C3-4DF3-96D2-981F166772AF")]
+        public readonly InputSlot<float> EmitPosYScatter = new InputSlot<float>();
+
     }
 }
--- Types/RenderQuad.cs
+++ Types/RenderQuad.cs
@@ -8,6 +8,8 @@ namespace T3.Operators.Types
         [Output(Guid = "1ff16183-13b9-4c8f-a82a-77e8be0c641b")]
         public readonly Slot<Command> Output = new Slot<Command>();
 
+        [Input(Guid = "018DAB29-DB3B-49EE-872B-9042C4C0BCED")]
+        public readonly InputSlot<System.Numerics.Vector4> Color = new InputSlot<System.Numerics.Vector4>();
 
     }
 }
