4dfe46990c1ce08e184d9bb3ab196725afd6b89e
pixtur <thomas.mann@framefield.com>
2019-12-15 17:31:40 +0100

    Added .color to [ImageQuadEmitter]

--- Types/ImageQuadEmitter.cs
+++ Types/ImageQuadEmitter.cs
@@ -1,37 +1,37 @@
-using SharpDX.Direct3D11;
-using T3.Core;
+using System;
 using T3.Core.Operator;
-using T3.Core.Operator.Helper;
 
 namespace T3.Operators.Types
 {
     public class ImageQuadEmitter : Instance<ImageQuadEmitter>
     {
         [Output(Guid = "23c5d230-4f31-44f8-9b65-5c8cf111bd45")]
-        public readonly Slot<Command> Command = new Slot<Command>();
+        public readonly Slot<T3.Core.Command> Command = new Slot<T3.Core.Command>();
 
-        [Input(Guid = "3fc23e8b-95bc-467d-8e60-3794b9135c34")]
-        public readonly InputSlot<string> ShaderFilename = new InputSlot<string>();
+
         [Input(Guid = "49a41a2a-39a4-46fd-ab70-5c8442a6fd61")]
-        public readonly InputSlot<ParticleSystem> ParticleSystem = new InputSlot<ParticleSystem>();
+        public readonly InputSlot<T3.Core.Operator.Helper.ParticleSystem> ParticleSystem = new InputSlot<T3.Core.Operator.Helper.ParticleSystem>();
 
         [Input(Guid = "d133b57d-4e2c-432a-8172-599e7f70e79e")]
-        public readonly MultiInputSlot<ShaderResourceView> ShaderResources = new MultiInputSlot<ShaderResourceView>();
+        public readonly MultiInputSlot<SharpDX.Direct3D11.ShaderResourceView> ShaderResources = new MultiInputSlot<SharpDX.Direct3D11.ShaderResourceView>();
 
-        [Input(Guid = "892C9FC8-817D-49D5-97D2-CB290C609AEB")]
+        [Input(Guid = "892c9fc8-817d-49d5-97d2-cb290c609aeb")]
         public readonly InputSlot<float> Size = new InputSlot<float>();
 
-        [Input(Guid = "241961F6-27F2-4D1D-85EE-51A819C3AE21")]
+        [Input(Guid = "241961f6-27f2-4d1d-85ee-51a819c3ae21")]
         public readonly InputSlot<float> Mass = new InputSlot<float>();
 
-        [Input(Guid = "6EF0891C-B6CD-4EEC-A570-11C38E889AD5")]
+        [Input(Guid = "6ef0891c-b6cd-4eec-a570-11c38e889ad5")]
         public readonly InputSlot<float> LifeTime = new InputSlot<float>();
 
-        [Input(Guid = "59213885-EBBB-41CF-A6BC-B0A4B657D22B")]
+        [Input(Guid = "59213885-ebbb-41cf-a6bc-b0a4b657d22b")]
         public readonly InputSlot<float> EmitPosY = new InputSlot<float>();
 
-        [Input(Guid = "F5FA3F1E-33C3-4DF3-96D2-981F166772AF")]
+        [Input(Guid = "f5fa3f1e-33c3-4df3-96d2-981f166772af")]
         public readonly InputSlot<float> EmitPosYScatter = new InputSlot<float>();
 
+        [Input(Guid = "98ae0757-7e78-40e1-9e9d-b807540dc4d4")]
+        public readonly InputSlot<System.Numerics.Vector4> Color = new InputSlot<System.Numerics.Vector4>();
     }
 }
+
