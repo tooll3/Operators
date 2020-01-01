e9982890b468389ed3d432bee008148e50dcfb17
cynic <daniel@szymanski.cc>
2020-01-01 21:28:34 +0100

    Fixed GetTextureSize for usage with FallbackSize

--- Types/GetTextureSize.cs
+++ Types/GetTextureSize.cs
@@ -1,18 +1,12 @@
 using SharpDX;
-using SharpDX.Direct3D;
-using SharpDX.Direct3D11;
 using T3.Core.Operator;
-using Vector2 = System.Numerics.Vector2;
 
 namespace T3.Operators.Types
 {
     public class GetTextureSize : Instance<GetTextureSize>
     {
-        // [Output(Guid = "1652BAC6-D496-4726-96F5-DB47927CCA8B")]
-        // public readonly Slot<System.Numerics.Vector2> Size = new Slot<System.Numerics.Vector2>();
-        
-        [Output(Guid = "BE16D5D3-4D21-4D5A-9E4C-C7B2779B6BDC")]
-        public readonly Slot<Size2> Size = new Slot<Size2>();
+        [Output(Guid = "be16d5d3-4d21-4d5a-9e4c-c7b2779b6bdc")]
+        public readonly Slot<SharpDX.Size2> Size = new Slot<SharpDX.Size2>();
 
         
         public GetTextureSize()
@@ -22,28 +16,19 @@ namespace T3.Operators.Types
 
         private void Update(EvaluationContext context)
         {
-            if (!Texture.DirtyFlag.IsDirty)
-                return; // nothing to do
-            
+            Size.Value = FallbackSize.GetValue(context);
+
             var texture = Texture.GetValue(context);
             if (texture != null)
             {
-                Size.Value = new Size2(
-                                         texture.Description.Width,
-                                         texture.Description.Height);
-                return;
-                
+                Size.Value = new Size2(texture.Description.Width, texture.Description.Height);
             }
-            Size.Value = new  Size2(1,1);
         }
 
-        // [Input(Guid = "10D5DEE1-A85C-4462-ABF4-401B0BCAF275")]
-        // public readonly InputSlot<float> XXX = new InputSlot<float>();
-        //
-        // [Input(Guid = "6B635BF9-E6A4-42BD-AD6F-A4C50C56E921")]
-        // public readonly InputSlot<ShaderResourceView> Srv = new InputSlot<ShaderResourceView>();
+        [Input(Guid = "8b15d8e1-10c7-41e1-84db-a85e31e0c909")]
+        public readonly InputSlot<SharpDX.Direct3D11.Texture2D> Texture = new InputSlot<SharpDX.Direct3D11.Texture2D>();
 
-        [Input(Guid = "8B15D8E1-10C7-41E1-84DB-A85E31E0C909")]
-        public readonly InputSlot<Texture2D> Texture = new InputSlot<Texture2D>();
+        [Input(Guid = "e0e3a635-424c-4bc7-8463-d06b4ae78244")]
+        public readonly InputSlot<SharpDX.Size2> FallbackSize = new InputSlot<SharpDX.Size2>();
     }
 }
