1650fee4cb9aa293202b9acfa292aa315aa91979
cynic <daniel@szymanski.cc>
2020-04-26 18:25:07 +0200

    Put particle shadow map example into own op

--- Types/Partial.cs
+++ Types/Partial.cs
@@ -10,9 +10,6 @@ namespace T3.Operators.Types.Id_a9080ad4_6cb2_4d5b_ad07_1cce239ee787
         [Output(Guid = "1668e648-7173-4d8b-9e0c-979772f5a396")]
         public readonly Slot<Texture2D> TextureOutput = new Slot<Texture2D>();
 
-        [Output(Guid = "9fccfe48-f1ca-42b0-9aef-e6d6e8f39155")]
-        public readonly Slot<Texture2D> TextureOutput2 = new Slot<Texture2D>();
-
 
     }
 }
--- Types/ParticleShadowmapExample.cs
+++ Types/ParticleShadowmapExample.cs
@@ -5,7 +5,7 @@ using T3.Core.Operator.Slots;
 
 namespace T3.Operators.Types.Id_7a5e8169_601e_4fd8_8ce4_bff669f50d37
 {
-    public class ParticleRenderPipeline : Instance<ParticleRenderPipeline>
+    public class ParticleShadowmapExample : Instance<ParticleShadowmapExample>
     {
         [Output(Guid = "229cf691-ee88-4e84-ad4f-90c51ecb14df")]
         public readonly Slot<Texture2D> TextureOutput = new Slot<Texture2D>();
