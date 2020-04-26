7c62b185ba5efe69b5c059b4199e04dc8a60c2b8
cynic <daniel@szymanski.cc>
2020-04-26 18:25:37 +0200

    Added new particle test op to experiment

---
+++ Types/ParticleTests.cs
@@ -0,0 +1,16 @@
+using SharpDX.Direct3D11;
+using T3.Core.Operator;
+using T3.Core.Operator.Attributes;
+using T3.Core.Operator.Slots;
+
+namespace T3.Operators.Types.Id_4f33f57d_ddca_44ea_96d2_8897ff5da39b
+{
+    public class ParticleTests : Instance<ParticleTests>
+    {
+        [Output(Guid = "fe6ded6a-e31f-4b3d-b6e1-28c53695a9fd")]
+        public readonly Slot<Texture2D> TextureOutput = new Slot<Texture2D>();
+
+
+    }
+}
+
