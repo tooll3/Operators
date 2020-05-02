3a5a0f880930ad914abbb7d0cbcd3fd2ebfff708
cynic <daniel@szymanski.cc>
2020-05-03 00:48:54 +0200

    AddPointCloud is now only doing the GPU setup
    The data to use is now passed in as parameter

--- Types/AddPointClound.cs
+++ Types/AddPointClound.cs
@@ -12,6 +12,15 @@ namespace T3.Operators.Types.Id_557fcca1_5406_4594_9dd2_9122d3b3a1e2
 
         [Input(Guid = "c138ad9e-5625-4ac1-b6f9-9fff7aba523e")]
         public readonly InputSlot<T3.Core.Operator.Helper.ParticleSystem> ParticleSystem = new InputSlot<T3.Core.Operator.Helper.ParticleSystem>();
+
+        [Input(Guid = "a014d4ca-001f-4a41-a239-63346652ddd5")]
+        public readonly InputSlot<int> Count = new InputSlot<int>();
+
+        [Input(Guid = "27ed0afb-b4d3-4212-8ae6-32e1f7bc14cb")]
+        public readonly InputSlot<SharpDX.Direct3D11.ShaderResourceView> Data = new InputSlot<SharpDX.Direct3D11.ShaderResourceView>();
+
+        [Input(Guid = "9f98e4a1-a612-4b81-9233-b8f6f34fdd3b")]
+        public readonly InputSlot<int> EmitterId = new InputSlot<int>();
     }
 }
 
