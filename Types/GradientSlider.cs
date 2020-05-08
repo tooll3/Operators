6194723b99913c8e0a342458ad216a4cb2668cf2
pixtur <thomas.mann@framefield.com>
2020-05-08 12:22:29 +0200

    Updated GradientSlider

--- Types/GradientSlider.cs
+++ Types/GradientSlider.cs
@@ -1,3 +1,4 @@
+using T3.Core.DataTypes;
 using T3.Core.Operator;
 using T3.Core.Operator.Attributes;
 using T3.Core.Operator.Slots;
@@ -16,21 +17,16 @@ namespace T3.Operators.Types.Id_8211249d_7a26_4ad0_8d84_56da72a5c536
 
         private void Update(EvaluationContext context)
         {
-            var min = Min.GetValue(context);
-            var max = Max.GetValue(context);
             var input = this.Input.GetValue(context);
-            Result.Value = (max - min) * input + min;
+            
+            //TODO: Add gradient sampling
+            Result.Value = input;
         }
 
+        [Input(Guid = "EFF10FAD-CF95-4133-91DB-EFC41258CD1B")]
+        public readonly InputSlot<Gradient> Gradient = new InputSlot<Gradient>();
+        
         [Input(Guid = "a4527e01-f19a-4200-85e5-00144f3ce061")]
         public readonly InputSlot<float> Input = new InputSlot<float>();
-        
-        [Input(Guid = "039be267-5b3f-45a0-ba8d-0fc591405eb0")]
-        public readonly InputSlot<float> Min = new InputSlot<float>();
-
-        [Input(Guid = "cd2744d4-c9cf-46c5-bdca-a60fd4f83ff6")]
-        public readonly InputSlot<float> Max = new InputSlot<float>();
-
-
     }
 }
