7c690ca7d5df031d78f6bb6afb39eced412b0950
pixtur <thomas.mann@framefield.com>
2020-05-17 17:07:04 +0200

    Added RasterEffect

---
+++ Types/RasterExample.cs
@@ -0,0 +1,20 @@
+using System;
+using SharpDX.Direct3D11;
+using T3.Core.Operator;
+using T3.Core.Operator.Attributes;
+using T3.Core.Operator.Slots;
+
+namespace T3.Operators.Types.Id_014b8d6f_c7f2_43b5_84a8_033356e440ef
+{
+    public class RasterExample : Instance<RasterExample>
+    {
+        [Output(Guid = "8eedd2e2-8806-4e97-9ca2-ec6d881e62fc")]
+        public readonly Slot<Texture2D> Output = new Slot<Texture2D>();
+
+
+        [Input(Guid = "73b68512-01e8-4e1b-9dc3-e6fed4737563")]
+        public readonly InputSlot<float> Float = new InputSlot<float>();
+
+    }
+}
+
