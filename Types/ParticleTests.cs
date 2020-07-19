0761e0978ded4cb69d9581fbd00eeb977309d04b
cynic <daniel@szymanski.cc>
2020-07-19 16:24:09 +0200

    Particle test op now exposes also genereated depth buffer as output

--- Types/ParticleTests.cs
+++ Types/ParticleTests.cs
@@ -10,6 +10,9 @@ namespace T3.Operators.Types.Id_4f33f57d_ddca_44ea_96d2_8897ff5da39b
         [Output(Guid = "fe6ded6a-e31f-4b3d-b6e1-28c53695a9fd")]
         public readonly Slot<Texture2D> TextureOutput = new Slot<Texture2D>();
 
+        [Output(Guid = "12c34caf-d266-405e-8038-95ff9c9834d7")]
+        public readonly Slot<SharpDX.Direct3D11.Texture2D> DepthBuffer = new Slot<SharpDX.Direct3D11.Texture2D>();
+
 
     }
 }
