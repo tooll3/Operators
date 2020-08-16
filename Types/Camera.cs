d0fc529784dd4061d148fe98caaaccdf213541c8
Thomas Mann <thomas.mann@framefield.com>
2020-08-16 15:43:17 +0200

    Add camera interaction

--- Types/Camera.cs
+++ Types/Camera.cs
@@ -4,11 +4,12 @@ using T3.Core;
 using T3.Core.Logging;
 using T3.Core.Operator;
 using T3.Core.Operator.Attributes;
+using T3.Core.Operator.Interfaces;
 using T3.Core.Operator.Slots;
 
 namespace T3.Operators.Types.Id_746d886c_5ab6_44b1_bb15_f3ce2fadf7e6
 {
-    public class Camera : Instance<Camera>
+    public class Camera : Instance<Camera>, ICamera
     {
         [Output(Guid = "2E1742D8-9BA3-4236-A0CD-A2B02C9F5924", DirtyFlagTrigger = DirtyFlagTrigger.Always)]
         public readonly Slot<Command> Output = new Slot<Command>();
@@ -44,6 +45,26 @@ namespace T3.Operators.Types.Id_746d886c_5ab6_44b1_bb15_f3ce2fadf7e6
             context.WorldToCamera = prevWorldToCamera;
         }
 
+        // Implement ICamera 
+        public System.Numerics.Vector3 CameraPosition
+        {
+            get { return Position.Value;} 
+            set { Animator.UpdateVector3InputValue(Position, value); }
+        }
+
+        public System.Numerics.Vector3 CameraTarget
+        {
+            get { return Target.Value;} 
+            set { Animator.UpdateVector3InputValue(Target, value); }
+        }
+
+        public float CameraRoll
+        {
+            get { return Roll.Value;} 
+            set { Animator.UpdateFloatInputValue(Roll, value); }
+
+        }
+
         [Input(Guid = "047B8FAE-468C-48A7-8F3A-5FAC8DD5B3C6")]
         public readonly InputSlot<Command> Command = new InputSlot<Command>();
         [Input(Guid = "313596CC-3854-436B-89DA-5FD40164CE76")]
@@ -60,5 +81,7 @@ namespace T3.Operators.Types.Id_746d886c_5ab6_44b1_bb15_f3ce2fadf7e6
         public readonly InputSlot<float> Fov = new InputSlot<float>();
         [Input(Guid = "764CA304-FC86-48A9-9C82-A04FAC7EADB2")]
         public readonly InputSlot<float> Roll = new InputSlot<float>();
+
+
     }
 }
