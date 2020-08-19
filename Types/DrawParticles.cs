72fb863f0c9ebbc54d1831ebf45cb912a2a80fe3
Thomas Mann <thomas.mann@framefield.com>
2020-08-19 23:31:04 +0200

    Added basic shading for particles spheres

--- Types/DrawParticles.cs
+++ Types/DrawParticles.cs
@@ -21,6 +21,18 @@ namespace T3.Operators.Types.Id_73a55607_c892_4a85_946b_e37354c4c0e4
         [Input(Guid = "610ac5df-aa5b-4967-8f06-a3e071ce1225")]
         public readonly InputSlot<System.Numerics.Vector4> Color = new InputSlot<System.Numerics.Vector4>();
 
+        [Input(Guid = "c34e46fd-b1be-4224-bd8a-32e418eed96c")]
+        public readonly InputSlot<System.Numerics.Vector3> LightPosition = new InputSlot<System.Numerics.Vector3>();
+
+        [Input(Guid = "b7cff360-cbb6-4d28-98a1-dd0fd31145f7")]
+        public readonly InputSlot<float> LightIntensity = new InputSlot<float>();
+
+        [Input(Guid = "1b9b4455-d510-4151-93c6-e03a5eb325b8")]
+        public readonly InputSlot<float> LightDecay = new InputSlot<float>();
+
+        [Input(Guid = "981e17e2-6f92-4651-90a5-7c6ee4f04438")]
+        public readonly InputSlot<float> RoundShading = new InputSlot<float>();
+
     }
 }
 
--- Types/ParticleUpdate.cs
+++ Types/ParticleUpdate.cs
@@ -23,5 +23,20 @@ namespace T3.Operators.Types.Id_c6fce5e7_391e_4d08_bdc2_4c4960a2296e
 
         [Input(Guid = "9cbd5aed-e175-4001-aadc-19817cd25c33")]
         public readonly InputSlot<int> FilterEmitter = new InputSlot<int>();
+
+        [Input(Guid = "ec44a127-17fe-46f8-ba07-b07c4f18c5b2")]
+        public readonly InputSlot<float> Frequency = new InputSlot<float>();
+
+        [Input(Guid = "13d679fa-cd25-44d5-b617-c9911f73c276")]
+        public readonly InputSlot<float> Amount = new InputSlot<float>();
+
+        [Input(Guid = "d277f1ca-2892-45e5-b154-11077df1af15")]
+        public readonly InputSlot<float> Phase = new InputSlot<float>();
+
+        [Input(Guid = "60549314-65ab-4bfd-a12e-0818d22a43f7")]
+        public readonly InputSlot<float> ParticleFriction = new InputSlot<float>();
+
+        [Input(Guid = "45d61d8d-5441-4633-9f97-d57f8e583e08")]
+        public readonly InputSlot<float> Variation = new InputSlot<float>();
     }
 }
