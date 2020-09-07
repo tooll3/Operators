b761ec4cd344cbcdc683d77b478ac24ad3bdd814
Thomas Mann <thomas.mann@framefield.com>
2020-09-07 20:57:09 +0200

    Scene13 -> Attempt to add ParticleSystem to context

--- Types/RectTextureEmit.cs
+++ Types/RectTextureEmit.cs
@@ -12,14 +12,14 @@ namespace T3.Operators.Types.Id_c6911113_9411_4706_ad16_9e7bf58ad6c6
         [Output(Guid = "4efe1aa1-fc4c-495d-a25d-bcffe6491611")]
         public readonly Slot<Command> Output = new Slot<Command>();
 
-        [Input(Guid = "765b2330-777c-4b7d-bfa0-15f4701bedae")]
-        public readonly InputSlot<T3.Core.DataTypes.ParticleSystem> ParticleSystem = new InputSlot<T3.Core.DataTypes.ParticleSystem>();
-
         [Input(Guid = "7d0e8a44-367c-4eb0-8fa3-d26a67a5ec35")]
         public readonly InputSlot<SharpDX.Direct3D11.Texture2D> Texture = new InputSlot<SharpDX.Direct3D11.Texture2D>();
 
         [Input(Guid = "d9e528ba-0aa1-42b2-8169-984d7a340228")]
         public readonly InputSlot<int> EmitCountPerFrame = new InputSlot<int>();
+
+        [Input(Guid = "765b2330-777c-4b7d-bfa0-15f4701bedae")]
+        public readonly InputSlot<T3.Core.DataTypes.ParticleSystem> ParticleSystem = new InputSlot<T3.Core.DataTypes.ParticleSystem>();
 
         [Input(Guid = "ddac1768-0073-4158-9929-9c309d902429")]
         public readonly InputSlot<System.Numerics.Vector2> LifeTime = new InputSlot<System.Numerics.Vector2>();
