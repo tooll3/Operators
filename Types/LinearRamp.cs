1402528ddd6d15b0c3ab2ff4dda88b2c3c15b315
Thomas Mann <thomas.mann@framefield.com>
2020-09-27 13:22:33 +0200

    Add [BoolTofloats] and [LFO] and use Gradient for LinearRamp

--- Types/LinearRamp.cs
+++ Types/LinearRamp.cs
@@ -28,17 +28,20 @@ namespace T3.Operators.Types.Id_2c3d2c26_ac45_42e9_8f13_6ea338333568
         [Input(Guid = "8169be8f-cb35-4900-b462-f2139b412d59")]
         public readonly InputSlot<float> Rotation = new InputSlot<float>();
 
-        [Input(Guid = "d9d6878b-29d4-40d9-b342-ed8f60178ec2")]
-        public readonly InputSlot<float> PingPong = new InputSlot<float>();
+        [Input(Guid = "5774969c-ef4d-482e-ab37-b3a84b09debb")]
+        public readonly InputSlot<bool> PingPong = new InputSlot<bool>();
 
-        [Input(Guid = "b2fe829e-10cb-46f3-95f2-1e9acb3bd052")]
-        public readonly InputSlot<float> SmoothGradient = new InputSlot<float>();
+        [Input(Guid = "7f3fe86d-f259-458a-908a-0a71d39ca678")]
+        public readonly InputSlot<bool> Repeat = new InputSlot<bool>();
 
         [Input(Guid = "53afc8d9-f417-4628-9a97-220bec62919f")]
         public readonly InputSlot<SharpDX.Size2> Resolution = new InputSlot<SharpDX.Size2>();
 
         [Input(Guid = "fbe7e415-5740-4f44-ad4e-32e01c6eb1ad")]
         public readonly InputSlot<float> Bias = new InputSlot<float>();
+
+        [Input(Guid = "e47e9e63-9c94-4c29-9555-2452fa498d57")]
+        public readonly InputSlot<T3.Core.DataTypes.Gradient> Gradient = new InputSlot<T3.Core.DataTypes.Gradient>();
     }
 }
 
