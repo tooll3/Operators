e6a6622447890ed2d8dc4983f7435bfdb1558157
Thomas Mann <thomas.mann@framefield.com>
2020-10-26 23:58:59 +0100

    Prepared LFO for custom Ui

--- Types/LFO.cs
+++ Types/LFO.cs
@@ -11,27 +11,6 @@ using T3.Core.Operator.Slots;
 using T3.Operators.Types.Id_58aa74af_32aa_4c46_8bb5_5811f16bf7f8;
 using T3.Operators.Types.Id_c3a18346_930c_4242_9e42_aa9b3a439395;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 namespace T3.Operators.Types.Id_c5e39c67_256f_4cb9_a635_b62a0d9c796c
 {
     public class LFO : Instance<LFO>
@@ -43,25 +22,21 @@ namespace T3.Operators.Types.Id_c5e39c67_256f_4cb9_a635_b62a0d9c796c
         {
             Result.UpdateAction = Update;
         }
-        
-        
 
         private void Update(EvaluationContext context)
         {
-
-            
-            var time = OverrideTime.IsConnected
-                           ? OverrideTime.GetValue(context)
-                           : EvaluationContext.BeatTime;
-
-            var rate = Rate.GetValue(context);
-            var phase = Phase.GetValue(context);
-            
-            var fraction =  (float)(((time * rate + phase ) % 1f / Ratio.GetValue(context)).Clamp(0f,1f));
-
+            LastFraction = GetFraction(context);
             var bias = Bias.GetValue(context);
-            float r = fraction;
-            switch ((Shapes)Shape.GetValue(context))
+            var shape = (Shapes)Shape.GetValue(context);
+            var biased = CalcNormalizedValueForFraction(LastFraction, shape, bias);
+
+            Result.Value = biased * Amplitude.GetValue(context) + Offset.GetValue(context);
+        }
+
+        public float CalcNormalizedValueForFraction(float fraction, Shapes shape, float bias)
+        {
+            float r;
+            switch (shape)
             {
                 case Shapes.Ramp:
                     r = fraction;
@@ -70,7 +45,7 @@ namespace T3.Operators.Types.Id_c5e39c67_256f_4cb9_a635_b62a0d9c796c
                     r = 1 - fraction;
                     break;
                 case Shapes.Sine:
-                    r = (float)Math.Sin(fraction * 2 * 3.141578f)/2 + 0.5f;
+                    r = (float)Math.Sin(fraction * 2 * 3.141578f) / 2 + 0.5f;
                     break;
                 case Shapes.Square:
                     r = fraction > 0.5f ? 1 : 0;
@@ -78,20 +53,33 @@ namespace T3.Operators.Types.Id_c5e39c67_256f_4cb9_a635_b62a0d9c796c
                 case Shapes.ZigZag:
                     r = fraction < 0.5f
                             ? (fraction * 2)
-                            : (1-(fraction - 0.5f) * 2);  
+                            : (1 - (fraction - 0.5f) * 2);
                     break;
-
+                default:
+                    r = 0;
+                    break;
             }
-            
-            var biased = bias >= 0 
-                             ? (float)Math.Pow(r, bias+1)
-                             : 1-(float)Math.Pow(1-r, 1-bias);    
-            
-            Result.Value = biased * Amplitude.GetValue(context) + Offset.GetValue(context);
+
+            var biased = bias >= 0
+                             ? (float)Math.Pow(r, bias + 1)
+                             : 1 - (float)Math.Pow(1 - r, 1 - bias);
+            return biased;
         }
 
+        public float GetFraction(EvaluationContext context)
+        {
+            var time = OverrideTime.IsConnected
+                           ? OverrideTime.GetValue(context)
+                           : EvaluationContext.BeatTime;
 
-        private enum Shapes
+            var rate = Rate.GetValue(context);
+            var phase = Phase.GetValue(context);
+            return (float)((time * rate + phase) % 1f / Ratio.GetValue(context)).Clamp(0f, 1f);
+        }
+
+        public float LastFraction;
+
+        public enum Shapes
         {
             Ramp,
             Saw,
@@ -103,29 +91,25 @@ namespace T3.Operators.Types.Id_c5e39c67_256f_4cb9_a635_b62a0d9c796c
         [Input(Guid = "4C38C34C-D992-47F1-BCB5-9BD13FC6474B", MappedType = typeof(Shapes))]
         public readonly InputSlot<int> Shape = new InputSlot<int>();
 
-
-
         [Input(Guid = "a4d48d80-936c-4bbb-a2e8-32f86edd4ab2")]
         public readonly InputSlot<float> Rate = new InputSlot<float>();
 
         [Input(Guid = "3b9e7272-ccf3-4fff-a079-5fcbb8a6c7d5")]
         public readonly InputSlot<float> Ratio = new InputSlot<float>();
 
-
         [Input(Guid = "36ae5b4b-62e9-49c0-b841-97394122cb1e")]
         public readonly InputSlot<float> Phase = new InputSlot<float>();
-        
+
         [Input(Guid = "8a5033c2-7d22-44d7-9472-d23677b11388")]
         public readonly InputSlot<float> Amplitude = new InputSlot<float>();
-        
+
         [Input(Guid = "126511E6-771D-4DD0-8A9D-1861C7B45D23")]
         public readonly InputSlot<float> Offset = new InputSlot<float>();
-        
+
         [Input(Guid = "3396DE1F-03AF-43EE-A43A-55016BEC70AE")]
         public readonly InputSlot<float> Bias = new InputSlot<float>();
 
         [Input(Guid = "76ca8a8b-f252-4687-805e-fb7a86a16567")]
         public readonly InputSlot<float> OverrideTime = new InputSlot<float>();
-
     }
 }
