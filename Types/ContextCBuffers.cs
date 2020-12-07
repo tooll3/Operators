886fe48a38fa864e0f2a4fab2899141e0a279f87
Thomas Mann <thomas.mann@framefield.com>
2020-12-07 18:09:57 +0100

    Fixed ContextCBuffer update and add some text cases for PointLight

--- Types/ContextCBuffers.cs
+++ Types/ContextCBuffers.cs
@@ -7,10 +7,10 @@ namespace T3.Operators.Types.Id_d32a5484_880c_41d4_88ea_6ee1a3e61f0b
 {
     public class ContextCBuffers : Instance<ContextCBuffers>
     {
-        [Output(Guid = "d4171c74-5a90-4fe9-8334-10f9701c284c")]
+        [Output(Guid = "d4171c74-5a90-4fe9-8334-10f9701c284c", DirtyFlagTrigger = DirtyFlagTrigger.Animated)]
         public readonly Slot<Buffer> FogParameters = new Slot<Buffer>();
 
-        [Output(Guid = "5cb8c86e-c3a6-434c-b30a-a107121436b2")]
+        [Output(Guid = "5cb8c86e-c3a6-434c-b30a-a107121436b2", DirtyFlagTrigger = DirtyFlagTrigger.Animated)]
         public readonly Slot<Buffer> PointLights = new Slot<Buffer>();
 
         public ContextCBuffers()
