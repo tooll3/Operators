928a6c2e66a1857f9dbe1aa04da84ac5622b686d
cynic <daniel@szymanski.cc>
2020-12-14 22:20:21 +0100

    Mapped DOF blur size and radius scale to input parameters

--- Types/DepthOfField.cs
+++ Types/DepthOfField.cs
@@ -28,6 +28,12 @@ namespace T3.Operators.Types.Id_2cd650a1_5b77_4040_895b_6049dc09206e
         [Input(Guid = "616b0d12-f4d6-4b54-9745-25c510cab04f")]
         public readonly InputSlot<float> FocusRange = new InputSlot<float>();
 
+        [Input(Guid = "30038b30-7247-4395-8fae-d69c2b4c0ec6")]
+        public readonly InputSlot<float> BlurSize = new InputSlot<float>();
+
+        [Input(Guid = "3ea66775-0864-40af-ab73-7f5321ef81aa")]
+        public readonly InputSlot<float> QualityScale = new InputSlot<float>();
+
         [Input(Guid = "f66e71e9-aef9-41da-b722-843951e0dbd5")]
         public readonly InputSlot<SharpDX.Direct3D11.Texture2D> OutputTexture = new InputSlot<SharpDX.Direct3D11.Texture2D>();
 
