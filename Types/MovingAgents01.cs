1063b53a572ba150fd9a04fcc72d3dc80474bbc8
Thomas Mann <thomas.mann@framefield.com>
2020-12-16 02:32:55 +0100

    Add first moving agents experiments

--- Types/MovingAgents01.cs
+++ Types/MovingAgents01.cs
@@ -5,7 +5,7 @@ using T3.Core.Operator.Slots;
 
 namespace T3.Operators.Types.Id_0eff6d8b_0fca_420d_839c_3e90dff76cb8
 {
-    public class EdgeOfChaos1 : Instance<EdgeOfChaos1>
+    public class MovingAgents01 : Instance<MovingAgents01>
     {
         [Output(Guid = "93a2479a-e42d-4411-94c4-5280dc8d12fc")]
         public readonly Slot<Texture2D> ImgOutput = new Slot<Texture2D>();
