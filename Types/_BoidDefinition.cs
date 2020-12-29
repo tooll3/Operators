4a0ef9487179b37f66446f385e6816e3d5059561
Thomas Mann <thomas.mann@framefield.com>
2020-12-29 19:03:32 +0100

    Prepare Boid implementation

---
+++ Types/_BoidDefinition.cs
@@ -0,0 +1,110 @@
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using T3.Core.DataTypes;
+using T3.Core.Operator;
+using T3.Core.Operator.Attributes;
+using T3.Core.Operator.Slots;
+
+namespace T3.Operators.Types.Id_6b8db50a_383c_486c_8065_3aefe8c85576
+{
+    //using BreedList = StructuredList<_SpeciesDefinition.Breed>;
+
+    public class _BoidDefinition : Instance<_BoidDefinition>
+    {
+        [Output(Guid = "bdab6d26-afc1-4432-8069-5dcf1567eeba")]
+        public readonly Slot<StructuredList> OutBuffer = new Slot<StructuredList>();
+        
+        public _BoidDefinition()
+        {
+            OutBuffer.UpdateAction = Update;
+            _slots = new List<IInputSlot>()
+                         {
+                             CohesionRadius,
+                             CohesionDrive,
+                             AlignmentRadius,
+                             AlignmentDrive,
+                             SeparationRadius,
+                             SeparationDrive,
+                         };                   
+        }
+                    
+                
+        private void Update(EvaluationContext context)
+        {
+            if (IsAnyInputDirty() || !_initialized)
+            {
+                _boids.TypedElements[0].CohesionRadius = CohesionRadius.GetValue(context);
+                _boids.TypedElements[0].CohesionDrive = CohesionDrive.GetValue(context);
+                _boids.TypedElements[0].AlignmentRadius = AlignmentRadius.GetValue(context);
+                _boids.TypedElements[0].AlignmentDrive = AlignmentDrive.GetValue(context);
+                _boids.TypedElements[0].SeparationRadius = SeparationRadius.GetValue(context);
+                _boids.TypedElements[0].SeparationDrive = SeparationDrive.GetValue(context);
+                _initialized = true;
+            }
+            OutBuffer.Value = _boids;
+        }
+        
+        [StructLayout(LayoutKind.Explicit, Size = 16 * 4)]
+        public struct Boid
+        {
+            [FieldOffset(0 * 4)]
+            public float CohesionRadius;
+            
+            [FieldOffset(1 * 4)]
+            public float CohesionDrive;
+
+            [FieldOffset(2 * 4)]
+            public float AlignmentRadius;
+            
+            [FieldOffset(3 * 4)]
+            public float AlignmentDrive;
+
+            [FieldOffset(4 * 4)]
+            public float SeparationRadius;
+            
+            [FieldOffset(5 * 4)]
+            public float SeparationDrive;
+            
+            [FieldOffset(6 * 4)]
+            public float _padding1;
+            
+            [FieldOffset(7 * 4)]
+            public float _padding2;
+        }
+
+        private bool IsAnyInputDirty()
+        {
+            foreach (var i in _slots)
+            {
+                if (i.DirtyFlag.IsDirty)
+                    return true;
+            }
+
+            return false;
+        }
+        
+        
+        private StructuredList<Boid> _boids = new StructuredList<Boid>(1);
+        private readonly List<IInputSlot> _slots;
+        private bool _initialized;
+        
+        [Input(Guid = "BB9A7B9E-05F2-4383-B8EB-C73F21125EA3")]
+        public readonly InputSlot<float> CohesionRadius = new InputSlot<float>();
+        
+        [Input(Guid = "44836F44-717A-4BB7-B115-15270DA62E1C")]
+        public readonly InputSlot<float> CohesionDrive = new InputSlot<float>();
+        
+        [Input(Guid = "D33DE75A-02AE-4FC3-99A7-B28227ACB531")]
+        public readonly InputSlot<float> AlignmentRadius = new InputSlot<float>();
+            
+        [Input(Guid = "43C6242B-8393-4E99-9B6A-97CE22D59E3D")]
+        public readonly InputSlot<float> AlignmentDrive = new InputSlot<float>();
+
+        [Input(Guid = "D894DD3D-7151-47D2-B665-D60318FE096D")]
+        public readonly InputSlot<float> SeparationRadius = new InputSlot<float>();
+            
+        [Input(Guid = "CBB63479-1A21-4427-9441-2E1DC73D3F3C")]
+        public readonly InputSlot<float> SeparationDrive = new InputSlot<float>();
+        
+    }
+}
---
+++ Types/_SimulateBoids.cs
@@ -0,0 +1,34 @@
+using SharpDX.Direct3D11;
+using T3.Core.Operator;
+using T3.Core.Operator.Attributes;
+using T3.Core.Operator.Slots;
+
+namespace T3.Operators.Types.Id_4f8c54a3_d146_4f7c_8628_17697b0201c9
+{
+    public class _SimulateBoids : Instance<_SimulateBoids>
+    {
+        [Output(Guid = "5f95c6f4-e654-4858-90fe-5dfa540e2186")]
+        public readonly Slot<Texture2D> ImgOutput = new Slot<Texture2D>();
+
+        [Input(Guid = "60edbf47-c594-4079-89cb-abffc2a5baa0")]
+        public readonly InputSlot<int> AgentCount = new InputSlot<int>();
+
+        [Input(Guid = "ff301ad3-74c9-4bef-88c8-ab883c76b67a")]
+        public readonly InputSlot<SharpDX.Size2> BlockCount = new InputSlot<SharpDX.Size2>();
+
+        [Input(Guid = "eb1f8e21-521a-41f4-a761-1c47680b6afc")]
+        public readonly InputSlot<T3.Core.DataTypes.BufferWithViews> BreedsBuffer = new InputSlot<T3.Core.DataTypes.BufferWithViews>();
+
+        [Input(Guid = "2d740120-1fe5-444f-80a0-2201c4a9bfcd")]
+        public readonly InputSlot<int> ComputeSteps = new InputSlot<int>();
+
+        [Input(Guid = "df993647-f384-4371-8196-2f52ba9e2d5a")]
+        public readonly InputSlot<bool> RestoreLayoutEnabled = new InputSlot<bool>();
+
+        [Input(Guid = "cf868ea8-6a73-4c93-b435-b4e3a1a95167")]
+        public readonly InputSlot<float> RestoreLayout = new InputSlot<float>();
+
+
+    }
+}
+
