f84956e9068bdd9fc6775992761f0cca97d6d6d2
Thomas Mann <thomas.mann@framefield.com>
2021-01-05 23:42:09 +0100

    Extracted attribute field fetching into generic method

--- Types/GetListItemAttribute.cs
+++ Types/GetListItemAttribute.cs
@@ -1,19 +1,9 @@
-using System;
-using System.Collections.Generic;
 using System.Reflection;
-using System.Resources;
-using Microsoft.Win32;
-using SharpDX;
-using T3.Core;
 using T3.Core.DataTypes;
 using T3.Core.Logging;
 using T3.Core.Operator;
 using T3.Core.Operator.Attributes;
 using T3.Core.Operator.Slots;
-using Point = T3.Core.DataTypes.Point;
-using Quaternion = System.Numerics.Quaternion;
-using Vector3 = System.Numerics.Vector3;
-using Vector4 = SharpDX.Vector4;
 
 namespace T3.Operators.Types.Id_37794826_a099_4af3_90f4_1e49092a09e1
 {
@@ -29,47 +19,9 @@ namespace T3.Operators.Types.Id_37794826_a099_4af3_90f4_1e49092a09e1
 
         private void Update(EvaluationContext context)
         {
-            var list = DataList.GetValue(context);
-            if (list == null || list.NumElements == 0)
-                return;
-
-            var index = ItemIndex.GetValue(context) % list.NumElements;
-
-            var item = list[index];
-            var fieldInfos = list.Type.GetFields();
-
-            var needToUpdateIndex = FieldIndex.DirtyFlag.IsDirty || OrFieldName.DirtyFlag.IsDirty;
-            if (needToUpdateIndex)
-            {
-                var requestedFieldIndex = FieldIndex.GetValue(context);
-                var requestedFieldName = OrFieldName.GetValue(context);
-
-                _field = null;
-                var fieldIndex = 0;
-                foreach (var field in fieldInfos)
-                {
-                    if (fieldIndex == requestedFieldIndex || field.Name == requestedFieldName)
-                    {
-                        if (field.GetValue(item) is float f)
-                        {
-                            _field = field;
-                            Result.Value = f;
-                            return;
-                        }
-                    }
-
-                    fieldIndex++;
-                }
-
-                Log.Warning($"There is not Float field for index {requestedFieldIndex} or name '{requestedFieldName}' in type {list.Type.Name}", SymbolChildId);
-            }
-            else if (_field?.GetValue(item) is float f)
-            {
-                Result.Value = f;
-            }
+            Result.Value = Test.GetFieldWithOfType<float>(context, DataList, ItemIndex, FieldIndex, OrFieldName, ref _field);
         }
 
-        //private int _fieldIndex = 0;
         private FieldInfo _field;
 
         [Input(Guid = "9CF1AE77-1E80-443A-BB07-F545C3D2E71D")]
@@ -83,5 +35,53 @@ namespace T3.Operators.Types.Id_37794826_a099_4af3_90f4_1e49092a09e1
 
         [Input(Guid = "C6B58AB1-7311-4CF9-B013-C021683FA159")]
         public readonly InputSlot<string> OrFieldName = new InputSlot<string>();
+    }
+    
+    
+    public static class Test
+    {
+        public static T GetFieldWithOfType<T>(EvaluationContext context, InputSlot<StructuredList> dataListInput, InputSlot<int> itemIndex, InputSlot<int> FieldIndex,
+                                              InputSlot<string> orFieldName, ref FieldInfo fieldRef)
+        {
+            var list = dataListInput.GetValue(context);
+            if (list == null || list.NumElements == 0)
+                return default;
+
+            var index = itemIndex.GetValue(context) % list.NumElements;
+
+            var item = list[index];
+            var fieldInfos = list.Type.GetFields();
+
+            var needToUpdateIndex = FieldIndex.DirtyFlag.IsDirty || orFieldName.DirtyFlag.IsDirty;
+            if (needToUpdateIndex)
+            {
+                var requestedFieldIndex = FieldIndex.GetValue(context);
+                var requestedFieldName = orFieldName.GetValue(context);
+
+                fieldRef = null;
+                var fieldIndex = 0;
+                foreach (var field in fieldInfos)
+                {
+                    if (fieldIndex == requestedFieldIndex || field.Name == requestedFieldName)
+                    {
+                        if (field.GetValue(item) is T f)
+                        {
+                            fieldRef = field;
+                            return f;
+                        }
+                    }
+
+                    fieldIndex++;
+                }
+
+                Log.Warning($"There is not Float field for index {requestedFieldIndex} or name '{requestedFieldName}' in type {list.Type.Name}");
+            }
+            else if (fieldRef?.GetValue(item) is T f)
+            {
+                return f;
+            }
+
+            return default;
+        }
     }
 }
