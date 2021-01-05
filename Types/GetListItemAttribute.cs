9e5e00a7660fd3a4021411d8a21ec2cdb21a6959
Thomas Mann <thomas.mann@framefield.com>
2021-01-05 22:49:41 +0100

    GetListItemAttribute caches accessor field

--- Types/GetListItemAttribute.cs
+++ Types/GetListItemAttribute.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Resources;
 using Microsoft.Win32;
 using SharpDX;
 using T3.Core;
 using T3.Core.DataTypes;
+using T3.Core.Logging;
 using T3.Core.Operator;
 using T3.Core.Operator.Attributes;
 using T3.Core.Operator.Slots;
@@ -33,36 +35,53 @@ namespace T3.Operators.Types.Id_37794826_a099_4af3_90f4_1e49092a09e1
 
             var index = ItemIndex.GetValue(context) % list.NumElements;
 
-            var requestedFieldIndex = FieldIndex.GetValue(context);
-            var requestedFieldName = OrFieldName.GetValue(context);
-            
-            var fieldIndex = 0;
-            foreach (var field in list.Type.GetFields())
+            var item = list[index];
+            var fieldInfos = list.Type.GetFields();
+
+            var needToUpdateIndex = FieldIndex.DirtyFlag.IsDirty || OrFieldName.DirtyFlag.IsDirty;
+            if (needToUpdateIndex)
             {
-                if (fieldIndex == requestedFieldIndex || field.Name == requestedFieldName)
+                var requestedFieldIndex = FieldIndex.GetValue(context);
+                var requestedFieldName = OrFieldName.GetValue(context);
+
+                _field = null;
+                var fieldIndex = 0;
+                foreach (var field in fieldInfos)
                 {
-                    var item = list[index];
-                    if (field.GetValue(item) is float f)
+                    if (fieldIndex == requestedFieldIndex || field.Name == requestedFieldName)
                     {
-                        Result.Value = f;
-                        return;
+                        if (field.GetValue(item) is float f)
+                        {
+                            _field = field;
+                            Result.Value = f;
+                            return;
+                        }
                     }
+
+                    fieldIndex++;
                 }
-                fieldIndex++;
+
+                Log.Warning($"There is not Float field for index {requestedFieldIndex} or name '{requestedFieldName}' in type {list.Type.Name}", SymbolChildId);
+            }
+            else if (_field?.GetValue(item) is float f)
+            {
+                Result.Value = f;
             }
         }
 
+        //private int _fieldIndex = 0;
+        private FieldInfo _field;
+
         [Input(Guid = "9CF1AE77-1E80-443A-BB07-F545C3D2E71D")]
         public readonly InputSlot<StructuredList> DataList = new InputSlot<StructuredList>();
-        
+
         [Input(Guid = "09F5501D-5F51-4406-B0CD-4C3E572A376F")]
         public readonly InputSlot<int> ItemIndex = new InputSlot<int>();
 
         [Input(Guid = "6A988C18-DFE1-4F52-9019-ED97BD61A6E8")]
         public readonly InputSlot<int> FieldIndex = new InputSlot<int>();
-        
+
         [Input(Guid = "C6B58AB1-7311-4CF9-B013-C021683FA159")]
         public readonly InputSlot<string> OrFieldName = new InputSlot<string>();
-
     }
 }
