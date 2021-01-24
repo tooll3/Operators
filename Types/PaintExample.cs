9691aafdcb37bad3e592608411f646135db27bfc
Thomas Mann <thomas.mann@framefield.com>
2021-01-24 15:39:53 +0100

    Cleaned up TestLab and Dashboard ops and created a bunch of *Example ops

--- Types/PaintExample.cs
+++ Types/PaintExample.cs
@@ -10,6 +10,9 @@ namespace T3.Operators.Types.Id_8e6ed99c_a3e0_42c0_9f81_a89b1e340757
         [Output(Guid = "8cedd2ef-75a2-46d9-8a07-02491389a89f")]
         public readonly Slot<Texture2D> ColorBuffer = new Slot<Texture2D>();
 
+        [Input(Guid = "cc4b0b55-59c9-4898-b4ad-01568510e336")]
+        public readonly InputSlot<bool> IsActve = new InputSlot<bool>();
+
 
     }
 }
--- Types/Transform.cs
+++ Types/Transform.cs
@@ -29,7 +29,7 @@ namespace T3.Operators.Types.Id_284d2183_197d_47fd_b130_873cced78b1c
         {
             TransformCallback?.Invoke(this, context);
 
-            var s = Scale.GetValue(context);
+            var s = Scale.GetValue(context) * UniformScale.GetValue(context);
             var r = Rotation.GetValue(context);
             float yaw = MathUtil.DegreesToRadians(r.Y);
             float pitch = MathUtil.DegreesToRadians(r.X);
@@ -46,12 +46,19 @@ namespace T3.Operators.Types.Id_284d2183_197d_47fd_b130_873cced78b1c
 
         [Input(Guid = "DCD066CE-AC44-4E76-85B3-78821245D9DC")]
         public readonly InputSlot<Command> Command = new InputSlot<Command>();
+        
         [Input(Guid = "B4A8C16D-5A0F-4867-AE03-92A675ABE709")]
         public readonly InputSlot<System.Numerics.Vector3> Translation = new InputSlot<System.Numerics.Vector3>();
+        
         [Input(Guid = "712ADB09-D249-4C91-86DB-3FEDF6B05971")]
         public readonly InputSlot<System.Numerics.Vector3> Rotation = new InputSlot<System.Numerics.Vector3>();
+        
         [Input(Guid = "DA4CD6C8-2307-45DA-9258-49C578025AA8")]
         public readonly InputSlot<System.Numerics.Vector3> Scale = new InputSlot<System.Numerics.Vector3>();
+
+        [Input(Guid = "A7B1E667-BCE3-4E76-A5B1-0955C118D0FC")]
+        public readonly InputSlot<float> UniformScale = new InputSlot<float>();
+
 
     }
 }
