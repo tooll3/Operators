8ec9594d64782098267fcdac6aa4a685321fa01d
Thomas Mann <thomas.mann@framefield.com>
2021-02-01 23:40:18 +0100

    Fixed invalidating values in GetBufferComponents

--- Types/GetBufferComponents.cs
+++ Types/GetBufferComponents.cs
@@ -32,6 +32,12 @@ namespace T3.Operators.Types.Id_80dff680_5abf_484a_b9e0_81d72f3b7aa4
                 ShaderResourceView.Value = bufferWithViews.Srv;
                 UnorderedAccessView.Value = bufferWithViews.Uav;
             }
+            else
+            {
+                Buffer.Value = null;
+                ShaderResourceView.Value = null;
+                UnorderedAccessView.Value = null;
+            }
         }
 
         [Input(Guid = "7a13b834-21e5-4cef-ad5b-23c3770ea763")]
