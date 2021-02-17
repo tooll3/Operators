7135b209f815592f74ff14f7411e398122775e5d
Thomas Mann <thomas.mann@framefield.com>
2021-02-17 22:19:00 +0100

    Add WrapPointPositions

--- Types/BoolToFloats.cs
+++ Types/BoolToFloats.cs
@@ -21,8 +21,6 @@ namespace T3.Operators.Types.Id_9db2fcbf_54b9_4222_878b_80d1a0dc6edf
             Result.Value = BoolValue.GetValue(context) 
                                ? ForTrue.GetValue(context) 
                                : ForFalse.GetValue(context);
-            //ForTrue.DirtyFlag.Clear();
-            //ForFalse.DirtyFlag.Clear();
         }
         
         [Input(Guid = "253b9ae4-fac5-4641-bf0c-d8614606a840")]
