56ce2df0d124469ae3c081e8ef258175a208f391
Thomas Mann <thomas.mann@framefield.com>
2021-04-02 21:25:16 +0200

    Reworking partial timing

--- Types/RadialGPoints.cs
+++ Types/RadialGPoints.cs
@@ -1,16 +1,29 @@
 using System;
 using T3.Core.Operator;
 using T3.Core.Operator.Attributes;
+using T3.Core.Operator.Interfaces;
 using T3.Core.Operator.Slots;
 
 namespace T3.Operators.Types.Id_3352d3a1_ab04_4d0a_bb43_da69095b73fd
 {
-    public class RadialGPoints : Instance<RadialGPoints>
+    public class RadialGPoints : Instance<RadialGPoints>, ITransformable
     {
 
         [Output(Guid = "d7605a96-adc6-4a2b-9ba4-33adef3b7f4c")]
-        public readonly Slot<T3.Core.DataTypes.BufferWithViews> OutBuffer = new Slot<T3.Core.DataTypes.BufferWithViews>();
+        public readonly TransformCallbackSlot<T3.Core.DataTypes.BufferWithViews> OutBuffer = new TransformCallbackSlot<T3.Core.DataTypes.BufferWithViews>();
 
+        public RadialGPoints()
+        {
+            OutBuffer.TransformableOp = this;
+        }
+        
+        System.Numerics.Vector3 ITransformable.Translation { get => Center.Value; set => Center.SetTypedInputValue(value); }
+        System.Numerics.Vector3 ITransformable.Rotation { get => System.Numerics.Vector3.Zero; set { } }
+        System.Numerics.Vector3 ITransformable.Scale { get => System.Numerics.Vector3.One; set { } }
+
+        public Action<ITransformable, EvaluationContext> TransformCallback { get => OutBuffer.TransformCallback; set => OutBuffer.TransformCallback = value; }
+        
+        
         [Input(Guid = "b654ffe2-d46e-4a62-89b3-a9692d5c6481")]
         public readonly InputSlot<int> Count = new InputSlot<int>();
 
