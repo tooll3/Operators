df3c32da357e82c0721ab325afb01d64fead876e
Thomas Mann <thomas.mann@framefield.com>
2021-05-17 00:41:49 +0200

    FirstVersion of DispersePoints

--- Types/DispersePoints.cs
+++ Types/DispersePoints.cs
@@ -19,6 +19,15 @@ namespace T3.Operators.Types.Id_7246323c_5cd5_4db1_af25_3e96ce385c23
 
         [Input(Guid = "fa0f6143-24a5-4277-a23a-3ac535f29fed")]
         public readonly InputSlot<float> Dispersion = new InputSlot<float>();
+
+        [Input(Guid = "dbb567d1-0b9d-4848-afe5-99b0ec436934")]
+        public readonly InputSlot<float> CellSize = new InputSlot<float>();
+
+        [Input(Guid = "7cc4cb31-81aa-40c4-a439-5c2ee5ca2868")]
+        public readonly InputSlot<float> ClampAccelleration = new InputSlot<float>();
+
+        [Input(Guid = "93f9dfed-4efd-43fb-ac70-6c43d78d0f0f")]
+        public readonly InputSlot<bool> IsEnabled = new InputSlot<bool>();
     }
 }
 
