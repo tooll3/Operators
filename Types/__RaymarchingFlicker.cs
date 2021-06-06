27010611b6c78b7d5dda15ba1ce4133f830b4d00
Thomas Mann <thomas.mann@framefield.com>
2021-06-06 19:33:45 +0200

    Cleaned up point namespace

--- Types/__RaymarchingFlicker.cs
+++ Types/__RaymarchingFlicker.cs
@@ -5,7 +5,7 @@ using T3.Core.Operator.Slots;
 
 namespace T3.Operators.Types.Id_0231df7a_0764_4ff1_b4a7_725c46052bac
 {
-    public class RaymarchingFlicker : Instance<RaymarchingFlicker>
+    public class __RaymarchingFlicker : Instance<__RaymarchingFlicker>
     {
         [Output(Guid = "a57859fb-6756-41fa-96de-6b6130a4b635")]
         public readonly Slot<Texture2D> TextureOutput = new Slot<Texture2D>();
